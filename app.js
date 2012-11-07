268b0103991146b2eb403d6b30e9622ecd2737f1
Myles Grant <myles@mylesgrant.com>
2012-11-07 15:08:20 -0800

    Cleanup

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+var app = require('express')(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
 
-server.listen(8080);
+var port = 8080;
+server.listen(port);
+console.log('Listening on port '+port);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+io.sockets.on('connection', function(socket){
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function(data){
+		console.log(data);
+	});
 });
