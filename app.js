4514033fa81ed9db8d4361e0326cdddb1d4b75c8
Myles Grant <myles@mylesgrant.com>
2012-11-08 11:29:44 -0800

    Now uses the node-irc plugin to open and maintain an irc connection

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	irc = require('irc');
+
+var client;
 
 var port = 8080;
 server.listen(port);
@@ -18,9 +21,52 @@ app.get('/', function(req, res){
 // Accept websocket connections
 io.sockets.on('connection', function(socket){
 	socket.on('set_prefs', function(prefs){
-		console.log(prefs);
+		if (client){
+			if (client.opt.server != prefs['hostname'] || client.opt.port != prefs['port'] || client.opt.password != prefs['serverPassword']){
+				client.disconnect("Goodbye");
 
-		socket.emit('connected');
+				client.opt.server = prefs['hostname'];
+				client.opt.port = prefs['port'];
+				client.opt.password = prefs['serverPassword'];
+
+				client.opt.nick = prefs['nickname'];
+				client.opt.realName = prefs['name'];
+
+				client.connect();
+			}
+			else{
+				if (client.opt.nick != prefs['nickname']) client.send("NICK", prefs['nickname']);
+
+				// TODO: handle changing real name somehow?
+
+				socket.emit('connected');
+			}
+		}
+		else{
+			client = new irc.Client(prefs['hostname'], prefs['nickname'], {
+				userName: 'cloudirc',
+				debug: true,
+
+				port: prefs['port'],
+				realName: prefs['name'],
+				password: prefs['serverPassword'],
+
+				channels: ['#cloudirc']
+			});
+
+			client.addListener('error', function(message){
+				console.log('error: ', message);
+			});
+
+			client.addListener('connect', function(){
+				console.log("Connected to IRC");
+				socket.emit('connected');
+			});
+		}
+	});
+
+	socket.on('disconnect', function(){
+		if (client) client.disconnect("Goodbye"); // Obviously, in the future, we should persist this
 	});
 });
 
