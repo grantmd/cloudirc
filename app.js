1309b37c4ba2ab16e5a5d50b513f85d641791f8d
Myles Grant <myles@mylesgrant.com>
2012-11-08 10:24:57 -0800

    Starting to play with sending events back and forth

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ app.get('/', function(req, res){
 
 // Accept websocket connections
 io.sockets.on('connection', function(socket){
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function(data){
-		console.log(data);
+	socket.on('set_prefs', function(prefs){
+		console.log(prefs);
+
+		socket.emit('connected');
 	});
 });
 
